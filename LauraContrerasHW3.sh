f1859a5876cd266876cf02750c38d23b979c6025
Laura Contreras <laura.contreras@du.edu>
2022-07-11 21:38:25 -0700

    Homework 3, July 11, 2022

--- LauraContrerasHW3.sh
+++ LauraContrerasHW3.sh
@@ -106,7 +106,15 @@ echo "Mean frequency using floating point arithemetics = $float_mean"
 # One can type lazy_commit file1 file2 ... filen  commit_message
 # and file will be added , commited and pushed to remote master using one lazy_commit command.
 function lazy_commit() {
-  git add --all
-  git commit -m "$2"
-  git push
-    }
+  if [ $# -lt 2 ]
+  then
+    echo "Not enough parameters"
+    return
+  fi
+  message="${@:-1}"
+  length=$(($#-1))
+  files=${@:1:$length}
+  git add $files
+  git commit -m "$message"
+  git push origin main
+  }
